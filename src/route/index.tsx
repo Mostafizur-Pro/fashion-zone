99601f3f8969941e872d6305769d807497cde9a9
Mostafizur-Pro <mostafizur0195@gmail.com>
2023-07-12 15:22:26 +0600

    react router dom use

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -6,9 +6,27 @@ const routes = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      //   {
+      //     path: "/products",
+      //     element: <Products />,
+      //   },
+      //   {
+      //     path: "/product-details/:id",
+      //     element: <ProductDetails />,
+      //   },
+      //   {
+      //     path: "/checkout",
+      //     element: <Checkout />,
+      //   },
+    ],
   },
   {
-    path: "/home",
+    path: "*",
     element: <Home />,
   },
 ]);
